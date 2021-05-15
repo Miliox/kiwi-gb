efd3414f5a7fbad08e0f0adc5b75a2a32dad7c1f
Emiliano Firmino <emiliano.firmino@gmail.com>
2021-05-15 16:00:48 +0200

    Reorder main
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -35,51 +35,6 @@ use timer::Timer;
 const FRAME_DURATION: Duration = Duration::from_nanos(1_000_000_000 / 60);
 
 fn main() {
-    let sdl_context = sdl2::init().unwrap();
-
-    let audio_subsystem = sdl_context.audio().unwrap();
-
-    let audio_spec = AudioSpecDesired { freq: Some(44_100), channels: Some(2), samples: Some(2048) };
-    let mut audio_ch1: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
-    let mut audio_ch2: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
-    let mut audio_ch3: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
-    let mut audio_ch4: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
-
-    audio_ch1.resume();
-    audio_ch2.resume();
-    audio_ch3.resume();
-    audio_ch4.resume();
-
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let scale = 4;
-    let width = (SCREEN_PIXEL_WIDTH * scale) as u32;
-    let height = (SCREEN_PIXEL_HEIGHT * scale) as u32;
-
-    let window = video_subsystem
-        .window("Kiwi", width, height)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    let texture_creator = canvas.texture_creator();
-    let mut texture = texture_creator
-        .create_texture(
-            Some(PixelFormatEnum::ARGB32),
-            TextureAccess::Static,
-            SCREEN_PIXEL_WIDTH as u32,
-            SCREEN_PIXEL_HEIGHT as u32,
-        )
-        .unwrap();
-
-    let mut frame_begin_timestamp = Instant::now();
-    let mut frame_overslept_duration = Duration::from_nanos(0);
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut frame_counter: u64 = 0;
-
     let cpu = Box::new(Cpu::default());
     let cpu: *mut Cpu = Box::into_raw(cpu);
 
@@ -153,6 +108,57 @@ fn main() {
         //    (*mmu).cartridge_rom[i] = bios::DMG_BIOS[i];
         //}
     }
+
+    let sdl_context = sdl2::init().unwrap();
+
+    let (mut audio_ch1, mut audio_ch2, mut audio_ch3, mut audio_ch4) = {
+        let audio_subsystem = sdl_context.audio().unwrap();
+
+        let audio_spec = AudioSpecDesired { freq: Some(44_100), channels: Some(2), samples: Some(2048) };
+        let mut audio_ch1: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
+        let mut audio_ch2: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
+        let mut audio_ch3: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
+        let mut audio_ch4: AudioQueue<i8> = audio_subsystem.open_queue(None, &audio_spec).unwrap();
+
+        audio_ch1.resume();
+        audio_ch2.resume();
+        audio_ch3.resume();
+        audio_ch4.resume();
+
+        (audio_ch1, audio_ch2, audio_ch3, audio_ch4)
+    };
+
+    let window = {
+        let scale = 4;
+        let width = (SCREEN_PIXEL_WIDTH * scale) as u32;
+        let height = (SCREEN_PIXEL_HEIGHT * scale) as u32;
+
+        let video_subsystem = sdl_context.video().unwrap();
+
+        video_subsystem
+            .window("Kiwi", width, height)
+            .position_centered()
+            .build()
+            .unwrap()
+    };
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    let texture_creator = canvas.texture_creator();
+    let mut texture = texture_creator
+        .create_texture(
+            Some(PixelFormatEnum::ARGB32),
+            TextureAccess::Static,
+            SCREEN_PIXEL_WIDTH as u32,
+            SCREEN_PIXEL_HEIGHT as u32,
+        )
+        .unwrap();
+
+    let mut frame_begin_timestamp = Instant::now();
+    let mut frame_overslept_duration = Duration::from_nanos(0);
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut frame_counter: u64 = 0;
 
     let mut ticks_counter: u64 = 0;
 
